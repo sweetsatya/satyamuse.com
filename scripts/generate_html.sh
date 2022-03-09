fe4548b173b103e5c6acc70ebb0b3ddeda0944e2
sweetsatya <satyasings@protonmail.com>
2022-03-09 19:18:46 +0100

    split out lyrics, css from template.html

---
+++ scripts/generate_html.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+#
+# Generate HTML from templates.
+#
+
+set -euo pipefail
+
+# insert $1 into $2 where marker $3 is
+insert() {
+    line=$(grep -n $3 $2 | cut -d ":" -f 1)
+    { head -n $(($line-1)) $2; cat $1; tail -n +$(($line+1)) $2; }
+}
+
+insert main.css template.html '{{style}}' > /tmp/index0.html
+insert lyrics.html /tmp/index0.html '{{lyrics}}' > index.html
